18c402b621b55003903475e49f5b64d4307c3f18
charlyy <cyoon90@yahoo.com>
2014-07-17 13:31:30 -0700

    changed arrow size and color

--- js/main.js
+++ js/main.js
@@ -16,19 +16,6 @@ $(document).ready( function() {
     	'scrollSpeed' : 1000
     });
 
-    
-
-    // Reveal Scroll Top Button when scrolling down
-
- //    var startY= $('#bio').position().top + $('#bio').outerHeight();
-	// $('.back_top').html($('#nav').html());
-	// $(window).scroll(function () {
-	//     if($(this).scrollTop() > startY ){
-	//         $('.back_top').slideDown();
-	//     }else{
-	//         $('.back_top').slideUp();
-	//     }
-	// });
 });
 
 	
