7b5ec9c89dd2bdf283f61dc0b9685d4b68b980d6
charlyy <cyoon90@yahoo.com>
2014-07-17 12:36:11 -0700

    fixed backgrounds, added resume link

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
+$(window).load(function() {
+  $("#loader").fadeOut(4200);
+})
+
+
 $(document).ready( function() {
+	
+	// Loading Gif & FadeIn all sections  
+    
+	
+
+
+    // Scrolling Speed
+    
     $('.navbar').smint({
     	'scrollSpeed' : 1000
     });
+
+    
+
+    // Reveal Scroll Top Button when scrolling down
+
+ //    var startY= $('#bio').position().top + $('#bio').outerHeight();
+	// $('.back_top').html($('#nav').html());
+	// $(window).scroll(function () {
+	//     if($(this).scrollTop() > startY ){
+	//         $('.back_top').slideDown();
+	//     }else{
+	//         $('.back_top').slideUp();
+	//     }
+	// });
 });
 
-$(document).ready(function() {
-	var startY= $('#bio').position().top + $('#bio').outerHeight();
-	$('.back_top').html($('#nav').html());
-	$(window).scroll(function () {
-	    if($(this).scrollTop() > startY ){
-	        $('.back_top').slideDown();
-	    }else{
-	        $('.back_top').slideUp();
-	    }
-	});
-});
+	
+
+
+
+
+
+  
 
 
