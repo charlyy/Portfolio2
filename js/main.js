526991c24739ff45edcf02864343014c66d369a9
charlyy <cyoon90@yahoo.com>
2014-07-17 14:06:20 -0700

    loading gif working properly

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-$(window).load(function() {
-  $("#loader").fadeOut(4200);
-})
+//  Remove Loading Gif Function   //
+
+setTimeout(function() {
+      $('#loader').remove();
+}, 2600);
+
 
 
 $(document).ready( function() {
-	
-	// Loading Gif & FadeIn all sections  
-    
-	
-
-
-    // Scrolling Speed
+		
+// Navigation  Scrolling Speed   //
     
     $('.navbar').smint({
     	'scrollSpeed' : 1000
     });
 
+    $('.header_content').hide(0).delay(500).fadeIn("slow")
 });
 
 	
